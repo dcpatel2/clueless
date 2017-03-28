c731e3798041c87e0d92d6913d5771a41939b9dd
dcpatel2 <dcpatel2@ncsu.edu>
2017-03-28 19:07:01 -0400

    Drupal testing 7:06

--- Unity/Assets/Scripts/Managers/GameManager.cs
+++ Unity/Assets/Scripts/Managers/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour {
     /// <summary>
     /// Game Piece Prefab that will be instantiated multiple times at runtime for each player.
     /// </summary>
+    /// Drupal testing
     public GameObject gamePiecePrefab;
 
 	// Use this for initialization
